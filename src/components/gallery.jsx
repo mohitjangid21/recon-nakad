4cebe2ffde7bf615279f6f36f436a3de1ab12750
Sachin Kumar <sachin.k@nakad.co>
2023-10-10 11:04:57 +0530

    initial commit

---
+++ src/components/gallery.jsx
@@ -0,0 +1,139 @@
+import { Table,TableBody,TableCell,TableContainer,TableHead,TableRow,Paper } from "@mui/material";
+import { Image } from "./image";
+
+export const Gallery = (props) => {
+  function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Balance confirmation Bulk email', 'Yes', 'Yes', 'Yes', 'Yes'),
+  createData('Ledger Request Bulk email','Yes', 'Yes', 'Yes', 'Yes'),
+  createData('Automated Ledger reconciliation', 'No', 'No', 'No', 'No'),
+  createData('Automated Payment advice reconciliation', 'No', 'No', 'No', 'Yes'),
+  createData('Accounts Payable recon insights', 'No', 'No', 'No', 'Yes'),
+  createData('Accounts Receivable recon insights', 'No', 'No', 'No', 'Yes'),
+  createData('Accounts Recon Signoff Report', 'No', 'No', 'No', 'Yes'),
+  createData('Dunning Letter Bulk email', 'No', 'No', 'Yes', 'Yes'),
+  createData('MSME status update in bulk', 'No', 'No', 'Yes', 'Yes'),
+  createData('Business Partners Addition ', '50', '500', 'Unlimited', 'Unlimited'),
+  createData('Free Integration Included', 'No', 'No', 'Yes', 'Yes'),
+  createData('Price (INR)', '', '5,000', '10,000', 'Contact Sales'),
+];
+
+  return (
+    <div id='portfolio' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Pricing</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
+        </div>
+        <div className='row'>
+          <div className='portfolio-items'>
+            {/* {props.data
+              ? props.data.map((d, i) => (
+                <div key={`${d.title}-${i}`} className='col-sm-6 col-md-4 col-lg-4'>
+                  <Image title={d.title} largeImage={d.largeImage} smallImage={d.smallImage} />
+                </div>
+              ))
+              : 'Loading...'} */}
+              {/* <BasicTable /> */}
+               <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow >
+            <TableCell className="fs_20 pricing_header">Features</TableCell>
+            <TableCell className="fs_20 pricing_header">FREE</TableCell>
+            <TableCell  className="fs_20 pricing_header">SILVER</TableCell>
+            <TableCell className="fs_20 pricing_header">GOLD</TableCell>
+            <TableCell  className="fs_20 pricing_header">PLATINUM</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row" className="fs_18">
+                {row.name}
+              </TableCell>
+              <TableCell className="fs_18">{row.calories}</TableCell>
+              <TableCell className="fs_18">{row.fat}</TableCell>
+              <TableCell className="fs_18">{row.carbs}</TableCell>
+              <TableCell  className="fs_18">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+// annnana
+
+
+// import * as React from 'react';
+// import Table from '@mui/material/Table';
+// import TableBody from '@mui/material/TableBody';
+// import TableCell from '@mui/material/TableCell';
+// import TableContainer from '@mui/material/TableContainer';
+// import TableHead from '@mui/material/TableHead';
+// import TableRow from '@mui/material/TableRow';
+// import Paper from '@mui/material/Paper';
+
+// function createData(name, calories, fat, carbs, protein) {
+//   return { name, calories, fat, carbs, protein };
+// }
+
+// const rows = [
+//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   createData('Eclair', 262, 16.0, 24, 6.0),
+//   createData('Cupcake', 305, 3.7, 67, 4.3),
+//   createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
+
+// export default function BasicTable() {
+//   return (
+//     <TableContainer component={Paper}>
+//       <Table sx={{ minWidth: 650 }} aria-label="simple table">
+//         <TableHead>
+//           <TableRow>
+//             <TableCell>Dessert (100g serving)</TableCell>
+//             <TableCell align="right">Calories</TableCell>
+//             <TableCell align="right">Fat&nbsp;(g)</TableCell>
+//             <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+//             <TableCell align="right">Protein&nbsp;(g)</TableCell>
+//           </TableRow>
+//         </TableHead>
+//         <TableBody>
+//           {rows.map((row) => (
+//             <TableRow
+//               key={row.name}
+//               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+//             >
+//               <TableCell component="th" scope="row">
+//                 {row.name}
+//               </TableCell>
+//               <TableCell align="right">{row.calories}</TableCell>
+//               <TableCell align="right">{row.fat}</TableCell>
+//               <TableCell align="right">{row.carbs}</TableCell>
+//               <TableCell align="right">{row.protein}</TableCell>
+//             </TableRow>
+//           ))}
+//         </TableBody>
+//       </Table>
+//     </TableContainer>
+//   );
+// }
+
+// export default Gallery
---
+++ src/components/navigation.jsx
@@ -0,0 +1,74 @@
+export const Navigation = (props) => {
+  return (
+    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+      <div className='container'>
+        <div className='navbar-header'>
+          <button
+            type='button'
+            className='navbar-toggle collapsed'
+            data-toggle='collapse'
+            data-target='#bs-example-navbar-collapse-1'
+          >
+            {' '}
+            <span className='sr-only'>Toggle navigation</span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+          </button>
+          <a className='navbar-brand page-scroll' href='#page-top'>
+           NAKAD
+          </a>{' '}
+        </div>
+
+        <div
+          className='collapse navbar-collapse'
+          id='bs-example-navbar-collapse-1'
+        >
+          <ul className='nav navbar-nav navbar-right'>
+            {/* <li>
+              <a href='#features' className='page-scroll'>
+                Features
+              </a>
+            </li> */}
+            <li>
+              <a href='#about' className='page-scroll'>
+                About
+              </a>
+            </li>
+            <li>
+              <a href='#services' className='page-scroll'>
+              Features
+              </a>
+            </li>
+            <li>
+              <a href='#portfolio' className='page-scroll'>
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a href='#testimonials' className='page-scroll'>
+                Testimonials
+              </a>
+            </li>
+            <li>
+              <a href='#team' className='page-scroll'>
+              Client
+              </a>
+            </li>
+            <li>
+              <a href='#contact' className='page-scroll'>
+                Contact
+              </a>
+            </li>
+            <li>
+              <a href='https://app-test.nakad.co/' className='page-scroll try_for_free' target="_blank">
+              Try For Free
+              </a>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
